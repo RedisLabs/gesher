e2886162639a0f2aa3f69f761764cfc1735eac35
Shaya Potter <shaya@redislabs.com>
2020-06-14 11:29:38 +0300

    apply comments from review with roey

--- pkg/admission-proxy/webhooks.go
+++ pkg/admission-proxy/webhooks.go
@@ -7,6 +7,7 @@ import (
 	"crypto/x509"
 	"encoding/base64"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -52,7 +53,7 @@ func findWebhooks(request *v1beta1.AdmissionRequest) []Webhook {
 	webhook[caBundle] = string(data)
 
 	if webhook[port] = os.Getenv("ADM_SERVICE_PORT"); webhook[port] == "" {
-		panic("failed to read env variable ADM_SERVICE_CABUNDLE")
+		panic("failed to read env variable ADM_SERVICE_PORT")
 	}
 	if _, err := strconv.Atoi(webhook[port]); err != nil {
 		panic(fmt.Errorf("failed to convert port to an int: %v", err))
@@ -71,7 +72,7 @@ func checkWebhooks(webhooks []Webhook, r *http.Request, body *bytes.Reader) *v1b
 	errCh := make(chan error, len(webhooks))
 	wg.Add(len(webhooks))
 	for _, webhook := range webhooks {
-		go dumbWebhook(webhook, wg, r, body, errCh)
+		go doWebhook(webhook, wg, r, body, errCh)
 	}
 
 	wg.Wait()
@@ -79,7 +80,9 @@ func checkWebhooks(webhooks []Webhook, r *http.Request, body *bytes.Reader) *v1b
 
 	var errs []error
 	for e := range errCh {
-		errs = append(errs, e)
+		if e != nil {
+			errs = append(errs, e)
+		}
 	}
 	if len(errs) == 0 {
 		return approved()
@@ -93,7 +96,7 @@ func checkWebhooks(webhooks []Webhook, r *http.Request, body *bytes.Reader) *v1b
 	return errToAdmissionResponse(errs[0])
 }
 
-func dumbWebhook(webhook Webhook, wg *sync.WaitGroup, r *http.Request, body *bytes.Reader, errCh chan error) {
+func doWebhook(webhook Webhook, wg *sync.WaitGroup, r *http.Request, body *bytes.Reader, errCh chan error) {
 	defer wg.Done()
 
 	url := fmt.Sprintf("https://%v.%v:%v%v", webhook[serviceName], webhook[namespace], webhook[port], webhook[path])
@@ -128,34 +131,16 @@ func dumbWebhook(webhook Webhook, wg *sync.WaitGroup, r *http.Request, body *byt
 func toFailure(name string, resp *http.Response, httpErr error, failurePolicy admv1beta1.FailurePolicyType) error {
 	log.V(2).Info(fmt.Sprintf("toFailure: %v: httpErr = %v", name, httpErr))
 	if httpErr != nil {
-		if failurePolicy == admv1beta1.Fail {
-			log.V(1).Info("returning httpErr as failurePolicy = Fail")
-			return fmt.Errorf("proxied webhook %v failed: %v", name, httpErr)
-		} else {
-			log.V(1).Info("httpErr, but returning nil as failurePolicy = Ignore")
-			return nil
-		}
+		return errToFailure(name, fmt.Errorf("http error: %v", httpErr), failurePolicy)
 	}
 
 	if resp.Body == nil {
-		if failurePolicy == admv1beta1.Fail {
-			log.V(1).Info("body is nil, returning error as failurePolicy = Fail")
-			return fmt.Errorf("proxied webhook %v returned empty result", name)
-		} else {
-			log.V(1).Info("body is nil, returning nil as failurePolicy = Ignore")
-			return nil
-		}
+		return errToFailure(name, errors.New("response body is nil"), failurePolicy)
 	}
 
 	data, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		if failurePolicy == admv1beta1.Fail {
-			log.V(1).Info("failed to read body, returning error as failurePolicy = Fail")
-			return fmt.Errorf("proxied webhook %v: failed to read response body", name, err)
-		} else {
-			log.V(1).Info("failed to read body, returning nil as failurePolicy = Ignore")
-			return nil
-		}
+		return errToFailure(name, fmt.Errorf("ReadAll failed: %v", err), failurePolicy)
 	}
 
 	log.V(2).Info(fmt.Sprintf("toFailure: resp.Body = %v", string(data)))
@@ -163,21 +148,27 @@ func toFailure(name string, resp *http.Response, httpErr error, failurePolicy ad
 	var responseAdmissionReview v1beta1.AdmissionReview
 	err = json.Unmarshal(data, &responseAdmissionReview)
 	if err != nil {
-		if failurePolicy == admv1beta1.Fail {
-			return fmt.Errorf("proxied webhook %v: failed to unmarshall json", name, err)
-		} else {
-			log.V(1).Info("failed to json unmarshall, returning nil as failurePolicy = Ignore")
-			return nil
-		}
+		return errToFailure(name, fmt.Errorf("json unmarshall failed: %v", err), failurePolicy)
 	}
 
 	log.V(2).Info(fmt.Sprintf("toFailure: unmarshalled response = %+v\n", responseAdmissionReview))
 
 	if !responseAdmissionReview.Response.Allowed {
-		return fmt.Errorf("proxied webhook %v denied the request: %v", name, err)
+		return fmt.Errorf("proxied webhook %v denied the request: %v", name, responseAdmissionReview.Response.Result.Message)
 	}
 
 	log.V(2).Info("toFailure: passed all test")
 
 	return nil
+}
+
+func errToFailure(name string, err error, failurePolicy admv1beta1.FailurePolicyType) error {
+	switch failurePolicy {
+	case admv1beta1.Fail:
+		log.V(1).Info(fmt.Sprintf("err = %v and FailurePolicy == Fail", err))
+		return fmt.Errorf("proxied webhook %v failed: %v", name, err)
+	default:
+		log.V(1).Info(fmt.Sprintf("err = %v and FailurePolicy == Ignore", err))
+		return nil
+	}
 }
